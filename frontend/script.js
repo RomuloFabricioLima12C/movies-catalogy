36e88197eb15e2e6f08ee0ba3662532046f3ed27
rml.og <162826903+RomuloFabricioLima12C@users.noreply.github.com>
2026-08-21 16:55:31 -0300

    commit segundo real

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+async function buscarFilmes() {
+    // acessar a rota GET do backend e exibir os filmes na tela
+    const resposta = await fetch("http://localhost:3067")
+    const filmes = await resposta.json()
+    const sectionFilmes = document.querySelector(".filmes")
+
+    filmes.forEach((filme) => {
+        sectionFilmes.innerHTML += `
+            <div>
+                <h2>${filme.title}</h2>
+                <p><strong>Gênero:</strong> ${filme.genre}</p>
+                <p><strong>Duração:</strong> ${filme.duration} minutos</p>
+                <p><strong>Classificação indicativa:</strong> ${filme.age_rating > 0 ? filme.age_rating + ' anos' : 'Livre'}</p>
+            </div>
+        `
+    })
+}
+
+buscarFilmes()
