1f0c77b2d5f632a1cee6283c9a4b1e5bda588672
rml.og <162826903+RomuloFabricioLima12C@users.noreply.github.com>
2026-08-24 15:01:26 -0300

    teste do codigo

--- index.js
+++ index.js
@@ -4,66 +4,67 @@ import cors from "cors"
 
 const app = express()
 
-app.get("/movies", (request, response) => {
-    const selectCommand = "SELECT * FROM filmes_RomuloFabricio"
-
-    sql.query(selectCommand, (error, data) => {
-        if(error) {
-            console.log(error)
-            return
-        }
-
-        response.json(data)
-    })
-})
-
-
-app.listen(3067,()=>{
-    console.log("Servidor rodando na porta 3067")
-})
-
-app.post("/movies", (request, response) => {
-    const { title, genre, duration, age_rating } = request.body
-
-    const insertCommand= "INSERT INTO filmes_RomuloFabricio(title,genre,duration,age_rating) VALUES (?,?,?,?)";
-
-    sql.query(insertCommand, [title,genre,duration,age_rating], (error) => {
-            if(error) {
-                console.log(error)
-                return
-            }
-
-            response.status(201).json({
-                message:"Filme cadastrado com sucesso!"
-            })
-
-        })
-
-})
-
-app.delete("/movies/:id", (request, response) => {
-    const {id} = request.params
-
-    const deleteCommand = "DELETE FROM filmes_RomuloFabricio WHERE id=?"
-
-    sql.query(deleteCommand, [id], (error) => {
-        if(error) {
-            console.log(error)
-            return
-        }
-
-        response.json({
-            message:"Filme removido com sucesso!"
-        })
-    })
-})
 
 app.use(cors())
 app.use(express.json())
+
 
 const sql = mysql2.createPool({
     host: "benserverplex.ddns.net",
     user: "aluno_projetos",
     password: "aluno@projeto",
     database: "alunos_filmes03TB"
+})
+
+
+app.get("/movies", (request, response) => {
+    const selectCommand = "SELECT * FROM filmes_RomuloFabricio"
+
+    sql.query(selectCommand, (error, data) => {
+        if (error) {
+            console.log(error)
+            return response.status(500).json({ error: "Erro ao buscar filmes" })
+        }
+
+        response.json(data)
+    })
+})
+
+app.post("/movies", (request, response) => {
+    const { title, genre, duration, age_rating } = request.body
+
+    const insertCommand = "INSERT INTO filmes_RomuloFabricio(title,genre,duration,age_rating) VALUES (?,?,?,?)"
+
+    sql.query(insertCommand, [title, genre, duration, age_rating], (error) => {
+        if (error) {
+            console.log(error)
+            return response.status(500).json({ error: "Erro ao cadastrar filme" })
+        }
+
+        response.status(201).json({
+            message: "Filme cadastrado com sucesso!"
+        })
+    })
+})
+
+app.delete("/movies/:id", (request, response) => {
+    const { id } = request.params
+
+    const deleteCommand = "DELETE FROM filmes_RomuloFabricio WHERE id=?"
+
+    sql.query(deleteCommand, [id], (error) => {
+        if (error) {
+            console.log(error)
+            return response.status(500).json({ error: "Erro ao deletar filme" })
+        }
+
+        response.json({
+            message: "Filme removido com sucesso!"
+        })
+    })
+})
+
+
+app.listen(3067, () => {
+    console.log("Servidor rodando na porta 3067")
 })
